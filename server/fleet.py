e4624887b69061c65c6462fa83342f36a9a9d15a
Richard Chew <rchewd2@gmail.com>
2016-03-18 13:59:38 -0400

    Cleaned up fleet, history is now linear

--- server/fleet.py
+++ server/fleet.py
@@ -13,7 +13,13 @@ class Dispatch:
 		self.route = route
 		self.dest = dest
 		self.wait_time = wait_time
-		self.route_distance = route.getDistance()
+		if not route is None:
+			self.route_distance = route.getDistance()
+		else:
+			self.route_distance = 0
+
+	def getStartTime(self):
+		return self.start
 
 	def getEndTime(self):
 		return self.end
@@ -33,52 +39,56 @@ def dispatch_from_task(task, start_time):
 	return Dispatch(start_time, start_time + task.getDuration(),
 		task.getType(), task.getRoute(), task.getDest(), start_time - task.getTimeOrdered())
 
+def idle_dispatch(time, loc):
+	return Dispatch(time, -1, "IDLE", None, loc, None)
+
 class Vehicle:
 	def __init__(self, uid, is_pev, loc):
 		self.uid = uid
 		self.is_pev = is_pev
 		self.loc = loc
 
-		self.history = []
-		self.current = -1
+		self.history = [idle_dispatch(0, loc)]
+		self.current = 0
 
 		## TODO representation here
 
 	def update(self, time):
-		while len(self.history) - 1 > self.current:
-			if self.history[self.current + 1].end <= time:
-				## update location
-				self.current += 1
-
-				## advance current
-				self.loc = self.history[self.current].dest
-			else:
+		## The purpose of this method is to set the current loc (in case we use it in the future)
+		## and to ensure that IDLE is appended if we finish everything.
+		print str(time)
+		while len(self.history) > self.current and self.history[self.current].end <= time:
+			if self.history[self.current].end == -1:
 				break
-			
-		## TODO implement - update task,
-		## set location if necessary
+			self.loc = self.history[self.current].dest ## TODO care about partial completion
+			self.current += 1
+		if self.current == len(self.history):
+			self.history.append(idle_dispatch(self.history[-1].end, self.loc))
+		self.check_valid()
 
 	def assign(self, task, time):
-		## TODO create the arrival Dispatch
-		## and add to todo
-		## create the passenger/fare dispatch
-		## and add to todo
-		if len(self.history) - 1 > self.current:
-			self.history.append(create_dispatch(self.soonestFreeAfter(time), self.history[-1].dest, task.getPickupLoc()))
-		else:
-			self.history.append(create_dispatch(time, self.loc, task.getPickupLoc()))
+		if self.history[-1].kind == "IDLE":
+			self.history[-1].end = time
+		self.history.append(create_dispatch(self.soonestFreeAfter(time), self.history[-1].dest, task.getPickupLoc()))
+
 		wait_time = self.soonestFreeAfter(time) - task.getTimeOrdered()
 		self.history.append(dispatch_from_task(task, self.soonestFreeAfter(time)))
 		return wait_time
+
+	def check_valid(self):
+		for i in xrange(len(self.history) - 1):
+			errstring = "[" + str(i) + "].end " + self.history[i].kind + "= " + str(self.history[i].end) + " != [" + str(i + 1) + "].start (" + self.history[i+1].kind + " = " + str(self.history[i + 1].start)
+			assert(self.history[i].end == self.history[i + 1].start), errstring
+			
 		
 
 	def soonestFreeAfter(self, t):
 		## return the soonest time that the PEV will
 		## be free after time t
-		if len(self.history) - 1 <= self.current:
+		if self.history[-1].end <= t:
 			return t
 		else:
-			return self.history[-1].getEndTime()
+			return self.history[-1].end
 
 	def getUID(self):
 		return self.uid
