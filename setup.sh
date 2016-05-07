045fb3fce8f06a52399e50aa5fb950803d524033
Richard Chew <rchewd2@gmail.com>
2016-05-07 19:52:23 -0400

    Moved setup script to upper directory and added mkdir db

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+mkdir db;
 (easy_install pip || (echo "Installing pip failed" && exit 1)) && pip install geopy && pip install -U googlemaps && open https://github.com/googlemaps/google-maps-services-python
