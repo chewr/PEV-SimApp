7b7fabacc83039fa154bd98d6acdd421512b6a43
Richard Chew <rchewd2@gmail.com>
2016-05-07 22:18:29 -0400

    envs stored by sim_uid

--- server/run_sim.py
+++ server/run_sim.py
@@ -3,10 +3,16 @@ import pev_sim
 import sim_util
 import json
 
+environs = {}
+
 def Run(sim_uid, fleet, maxDist, freq, dur):
 	start = 0
 	end = start + dur
 	pickups = dynamic_trips.TripRandomizer().assembleTripSim(maxDist, freq, maxDist, freq, start, end)
-	env = pev_sim.Sim_env(fleet, None, (42.3602595,-71.0873766))
+	if sim_uid in environs:
+		env = environs[sim_end]
+	else:
+		env = pev_sim.Sim_env(fleet, None, (42.3602595,-71.0873766))
+		environs[env.sim_uid] = env
 	env.schedule(None, pickups)
 	return json.dumps(env, default=sim_util.default_json, separators=(',', ':'), indent=4)
