d52bcb90b9bbc29d02ea2efee411f9dd6bd4697e
Richard Chew <rchewd2@gmail.com>
2016-05-10 10:12:51 -0400

    Added heuristics to pairing alg

--- server/fsched.py
+++ server/fsched.py
@@ -4,13 +4,25 @@
 import fleet
 from sets import Set
 import sim_util
+import routes
 
 def linearSpeedEst(a, b):
 	## distance estimate based on 3 m/s movement of straight-line distance
 	return int(sim_util.ll_dist_m(a, b) / 3)
 
+def nilHeuristic(a, b):
+	return 0
+
+def perfectHeuristic(a, b):
+	rte = routes.RouteFinder().get_dirs(a, b)
+	if rte:
+		return rte.getDuration()
+	else:
+		return 99999
+	
+
 def assign(time, task, fleet):
-	## strawman - assign task to soonest free member
+	hst = linearSpeedEst
 	illegal = Set([])
 	while len(illegal) < len(fleet.vehicles):
 		assignee = None
@@ -18,7 +30,7 @@ def assign(time, task, fleet):
 			if not pev.getUID() in illegal:
 				if assignee is None:
 					assignee = pev
-				elif pev.soonestArrivalAfter(time, task.getPickupLoc(), heuristic = linearSpeedEst) < assignee.soonestArrivalAfter(time, task.getPickupLoc(), heuristic = linearSpeedEst):
+				elif pev.soonestArrivalAfter(time, task.getPickupLoc(), heuristic = hst) < assignee.soonestArrivalAfter(time, task.getPickupLoc(), heuristic = hst):
 					assignee = pev
 		try: 
 			wait_time = assignee.assign(task, time)
